2c77b9d5321420c9f77d126c9f40cb19db495278
jordan2551 <jordan1158@gmail.com>
2017-10-09 12:39:44 -0400

    deploy

--- db/migrate/20171005071247_remove_is_assignment_form_task.rb
+++
@@ -1,8 +0,0 @@
-class RemoveIsAssignmentFormTask < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :tasks, :is_assignment, :boolean
-    remove_column :tasks, :is_exam, :boolean
-    remove_column :tasks, :is_quiz, :boolean
-
-  end
-end
